0a8e60a28b4a5cfe9ca197bbc90e2f727df55bcd
Terry <twang1010@hotmail.com>
2018-04-08 14:02:59 -0400

    day 3 orm partial

---
+++ www/model.py
@@ -0,0 +1,24 @@
+from orm import Model, StringField, IntegerField
+
+class User(Model):
+    __table__ = 'users'
+
+    id = IntegerField(primary_key=True)
+    name = StringField()
+
+
+class Model(dict, metaclass=ModelMetaclass):
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
